6908f34c4945f4ae1a63198ed1dc4e18d0df0ae2
sampath surineni <ssurineni@salesforce.com>
2023-08-29 20:12:30 +0530

    replaced https://jscomplete.com/playground/rgs3.6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,81 @@
 "use client"
-
 import { useState } from "react";
-const StarsDisplay = (props) => (
-  <>
-    {utils.range(1, props.stars).map(star =>
-      <div className="star" key={star} />
-    )}
-  </>
-)
-const PlayNumber = (props) => (
-  <button className="number"
-    style={{ backgroundColor: colors[props.status] }}
-    onClick={() => props.onClick(props.number, props.status)}>{props.number}</button>
-)
 
-
+// STAR MATCH - V6
 export default function Home() {
   return (
     <StarMatch></StarMatch>)
 }
+const StarsDisplay = props => (
+  <>
+    {utils.range(1, props.count).map(starId => (
+      <div key={starId} className="star" />
+    ))}
+  </>
+);
+
+const PlayNumber = props => (
+  <button
+    className="number"
+    style={{backgroundColor: colors[props.status]}}
+    onClick={() => props.onClick(props.number, props.status)}
+  >
+    {props.number}
+  </button>
+);
+
+const PlayAgain = props => (
+	<div className="game-done">
+	  <button onClick={props.onClick}>Play Again</button>
+	</div>
+);
 
 const StarMatch = () => {
   const [stars, setStars] = useState(utils.random(1, 9));
-  const [avlblNums, setAvlblNums] = useState(utils.range(1, 9));
-  const [cddNums, setCddNums] = useState([]);
-  const cddsAreX = utils.sum(cddNums) > stars;
-  const numberStatus = (number) => {
-    if (!avlblNums.includes(number)) {
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = useState([]);
+
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+  const gameIsDone = availableNums.length === 0;
+  
+  const resetGame = () => {
+  	setStars(utils.random(1, 9));
+    setAvailableNums(utils.range(1, 9));
+    setCandidateNums([]);
+  };
+
+  const numberStatus = number => {
+    if (!availableNums.includes(number)) {
       return 'used';
     }
-    if (cddNums.includes(number)) {
-      return cddsAreX ? 'wrong' : 'candidate';
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? 'wrong' : 'candidate';
     }
     return 'available';
-  }
+  };
 
   const onNumberClick = (number, currentStatus) => {
     if (currentStatus === 'used') {
       return;
     }
-    const newCddNums = currentStatus === 'available'
-      ? cddNums.concat(number)
-      : cddNums.filter(cddNum => cddNum !== number);
-    if (utils.sum(newCddNums) !== stars) {
-      setCddNums(newCddNums);
+
+		const newCandidateNums =
+      currentStatus === 'available'
+        ? candidateNums.concat(number)
+        : candidateNums.filter(cn => cn !== number);
+
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
     } else {
-      const newAvlblNums = avlblNums.filter(avlblNum => !newCddNums.includes(avlblNum));
-      setStars(utils.randomSumIn(newAvlblNums, 9));
-      setAvlblNums(newAvlblNums);
-      setCddNums([]);
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
     }
-  }
+  };
+
   return (
     <div className="game">
       <div className="help">
@@ -58,14 +83,20 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          <StarsDisplay stars={stars} />
+          {gameIsDone ? (
+          	<PlayAgain onClick={resetGame} />
+          ) : (
+          	<StarsDisplay count={stars} />
+          )}
         </div>
         <div className="right">
           {utils.range(1, 9).map(number => (
-            <PlayNumber key={number}
-              number={number}
+            <PlayNumber
+              key={number}
               status={numberStatus(number)}
-              onClick={onNumberClick} />
+              number={number}
+              onClick={onNumberClick}
+            />
           ))}
         </div>
       </div>
@@ -88,7 +119,7 @@ const utils = {
   sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
 
   // create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+  range: (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i),
 
   // pick a random number between min and max (edges included)
   random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
@@ -111,3 +142,7 @@ const utils = {
     return sums[utils.random(0, sums.length - 1)];
   },
 };
+
+
+// *** The React 18 way:
+// root.render(<StarMatch />);
